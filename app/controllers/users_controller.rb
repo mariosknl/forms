7fb2b585899d1d009db708cd75b46ef8e9296e81
mariosknl <mariosknl@outlook.com>
2020-05-05 12:39:04 +0300

    first step finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
+  root 'users#new'
   resources :users, only: [:new, :create]
 end
